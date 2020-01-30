936a651e2269ddd1cb6b28f047172314203f52ba
sandormatyas <sandormatyas7@gmail.com>
2020-01-30 10:25:09 +0100

    finished pair programming

--- src/main/java/com/codecool/datastructure/SinglyLinkedList.java
+++ src/main/java/com/codecool/datastructure/SinglyLinkedList.java
@@ -25,25 +25,55 @@ public class SinglyLinkedList {
     }
 
     private Link head;
+    private Link tail;
+    private int size = 0;
 
     public SinglyLinkedList() {
     }
 
     // Returns the number at 'index'.
     public int access(int index) {
-        return 0;
+        Link tempLink = head;
+        int currentIndex = 0;
+        while (currentIndex != index) {
+            tempLink = tempLink.getNext();
+            currentIndex++;
+        }
+        return tempLink.getValue();
     }
-
     // Returns the index of 'number' if it's in the array, otherwise -1;
     public int search(int number) {
-        return 0;
+        Link tempLink = head;
+        int currentIndex = 0;
+        while(tempLink.getValue() != number && currentIndex < size) {
+            tempLink = tempLink.getNext();
+            currentIndex ++;
+        }
+        return currentIndex < size ? currentIndex : -1;
     }
 
     // Inserts 'number' at 'index' into the array shifting elements if necessary.
     //
     // e.g. the result of inserting 42 at index 3 into [0, 1, 2, 3, 4] is [0, 1, 2, 42, 3, 4]
     public void insert(int index, int number) {
+        Link tempLink = head;
+        if (index == 0) {
+            this.head = new Link(number);
+            head.setNext(tempLink);
+        } else {
+            int currentIndex = 0;
+            while (currentIndex + 1 != index) {
+                tempLink = tempLink.getNext();
+                currentIndex++;
+
+            }
+            Link newLink = new Link(number);
+            newLink.setNext(tempLink.getNext());
+            tempLink.setNext(newLink);
+        }
+        size++;
     }
+
 
     // Deletes the element at 'index' from the array.
     //
